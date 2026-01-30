7c016feff990bc587ee53390945aae918d8c43d4
boning-zhu <boning.zhu@ucalgary.ca>
2026-01-30 13:44:28 -0700

    Create question4.py

---
+++ question4.py
@@ -0,0 +1,21 @@
+# Question 4: Sorted Search with Conditions
+
+from random import random
+
+values = [random() for i in range(20)]
+x = random()
+
+values.sort()
+
+indices = []
+for i in range(len(values)):
+    if values[i] >= x:
+        indices.append(i)
+
+print("Sorted values:", values)
+print("x:", x)
+
+if len(indices) > 0:
+    print("First matching index:", indices[0])
+else:
+    print("No value >= x")
